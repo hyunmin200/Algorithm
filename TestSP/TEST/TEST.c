c43b611d7e71fbf4a5043c005e093f14f9955a53
황현민 <hwanghyunmin0650@gmail.com>
2023-03-08 01:56:39 +0900

    TestSP에서 백준 문제 풀기

--- TestSP/TEST/TEST.c
+++ TestSP/TEST/TEST.c
@@ -1,37 +1,16 @@
 #include <stdio.h>
-
-int main() {
-    int a[15][15] = { 0, };
-    int num = 1, x = 0, y = 0;
-    int i, k, j;
-    int size, sign = 1;
-
-    printf("크기를 입력해주세요(MAX 14) : ");
-    scanf("%d", &size);
-
-    for (k = 0; k < size; ++k) {
-        a[y][x] = num++;
-        x += sign;
-    }
-    x -= 1;
-    for (i = size - 1; i > 0; --i) {
-        for (j = 0; j < i; ++j) {
-            y += sign;
-            a[y][x] = num++;
+#include <string.h>
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        for (int t = n; t > i+1; t--) {
+            printf(" ");
         }
-        sign *= -1;
-        for (k = 0; k < i; ++k) {
-            x += sign;
-            a[y][x] = num++;
+        for (int j = 0; j <= i * 2; j++) {
+            printf("*");
         }
+        printf("\n");
     }
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            printf("%3d ", a[i][j]);
-        }
-        printf("\n\n");
-    }
-
     return 0;
 }
